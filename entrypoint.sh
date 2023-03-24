7e8a1bb11b816cd937fe90c2c476cd789310353a
tarneaux <tarneo@tarneo.fr>
2023-03-24 22:16:24 +0100

    Add some things

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,9 @@ BUILD_PATH=/usr/share/nginx/html
 
 REPOS_PATH=/repos
 
+CONFIG_PATH=/config
 
--z "$BASEURL" && echo "BASEURL not set" && exit 1
-
-
-
-mkdir $BUILD_PATH
-
+[[ -z "$BASEURL" ]] && echo "BASEURL not set" && exit 1
 
 cd $REPOS_PATH
 repos_list=$(ls -d *)
@@ -35,5 +31,15 @@ echo $stagit_index_args_list
 
 stagit-index $stagit_index_args_list > index.html
 
+ln -s $CONFIG_PATH/style.css $BUILD_PATH/style.css
+ln -s $CONFIG_PATH/logo.png $BUILD_PATH/logo.png
+ln -s $CONFIG_PATH/favicon.ico $BUILD_PATH/favicon.ico
+
+# We also want the configs in all of the repos
+for repo in $repos_list; do
+    ln -s $CONFIG_PATH/style.css $BUILD_PATH/$repo/style.css
+    ln -s $CONFIG_PATH/logo.png $BUILD_PATH/$repo/logo.png
+    ln -s $CONFIG_PATH/favicon.ico $BUILD_PATH/$repo/favicon.ico
+done
 
 exec nginx -g "daemon off;"
