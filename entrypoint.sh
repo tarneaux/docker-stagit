1e0b223d39b3fc038e6cdfe18a270565adffb81c
tarneaux <tarneo@tarneo.fr>
2023-03-25 09:29:06 +0100

    Clean up entrypoint and make it list hidden repos

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,40 +8,54 @@ CONFIG_PATH=/config
 
 [[ -z "$BASEURL" ]] && echo "BASEURL not set" && exit 1
 
-cd $REPOS_PATH
-repos_list=$(ls -d *)
-cd -
+
+scan_repos() {
+    cd $REPOS_PATH
+    repos_list=$(ls -d */ .*/) # List all repos, including ones starting with a dot (hidden)
+    cd -
+}
+
+scan_repos
 
 [[ -z "$repos_list" ]] && echo "No repos found" && exit 1
 
-cd $BUILD_PATH
+build_repo() {
+    echo "Building $1"
+    mkdir $1
+    echo $REPOS_PATH/$1
+    (cd $repo; stagit -u $BASEURL $REPOS_PATH/$1)
+    echo "Done building $1"
+}
 
-for repo in $repos_list; do
-    echo "Building $repo"
-    mkdir $repo
-    echo $REPOS_PATH/$repo
-    (cd $repo; stagit -u $BASEURL $REPOS_PATH/$repo)
-    echo "Done building $repo"
-done
+build() {
+    scan_repos
+    cd $BUILD_PATH
 
-stagit_index_args_list=""
-for repo in $repos_list; do
-    stagit_index_args_list="$stagit_index_args_list $REPOS_PATH/$repo"
-done
+    for repo in $repos_list; do
+        build_repo $repo
+    done
 
-echo $stagit_index_args_list
+    stagit_index_args_list=""
+    for repo in $repos_list; do
+        stagit_index_args_list="$stagit_index_args_list $REPOS_PATH/$repo"
+    done
 
-stagit-index $stagit_index_args_list > index.html
+    echo $stagit_index_args_list
 
-ln -s $CONFIG_PATH/style.css $BUILD_PATH/style.css
-ln -s $CONFIG_PATH/logo.png $BUILD_PATH/logo.png
-ln -s $CONFIG_PATH/favicon.ico $BUILD_PATH/favicon.ico
+    stagit-index $stagit_index_args_list > index.html
 
-# We also want the configs in all of the repos
-for repo in $repos_list; do
-    ln -s $CONFIG_PATH/style.css $BUILD_PATH/$repo/style.css
-    ln -s $CONFIG_PATH/logo.png $BUILD_PATH/$repo/logo.png
-    ln -s $CONFIG_PATH/favicon.ico $BUILD_PATH/$repo/favicon.ico
-done
+    ln -s $CONFIG_PATH/style.css $BUILD_PATH/style.css
+    ln -s $CONFIG_PATH/logo.png $BUILD_PATH/logo.png
+    ln -s $CONFIG_PATH/favicon.ico $BUILD_PATH/favicon.ico
+
+    # We also want the configs in all of the repos
+    for repo in $repos_list; do
+        ln -s $CONFIG_PATH/style.css $BUILD_PATH/$repo/style.css
+        ln -s $CONFIG_PATH/logo.png $BUILD_PATH/$repo/logo.png
+        ln -s $CONFIG_PATH/favicon.ico $BUILD_PATH/$repo/favicon.ico
+    done
+}
+
+build
 
 exec nginx -g "daemon off;"
